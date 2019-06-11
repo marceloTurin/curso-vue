d5ccc8525f25becfb40fc50373a888be65fce993
Marcelo Sousa <marcelo.sousa@lbca.com.br>
2019-06-11 16:46:40 -0300

    Condicionais e Loop

--- script.js
+++ script.js
@@ -1,7 +1,21 @@
 //Instanciando objeto do vue
-let app = new Vue({
-	el: '#app', //Defenindo id do elemento	
+let placar = new Vue({
+	el: '#placar', //Defenindo id do elemento	
 	data:{ 
-		frase:'Olá Mundo! <strong>Marcelo</strong>'
+		n: 0
+	}
+})
+
+
+let lista = new Vue({
+	el: '#lista', //Defenindo id do elemento	
+	data:{ 
+		nomes: [
+			{nome:'Marcelo',sobrenome: 'Sousa', idade: '21'},
+			{nome:'Antonio',sobrenome: 'da Silva', idade: '18'},
+			{nome:'Paulo',sobrenome: 'de Tasso', idade: '25'},
+			{nome:'Maria',sobrenome: 'de Sousa', idade: '23'},
+				
+		]
 	}
 })
