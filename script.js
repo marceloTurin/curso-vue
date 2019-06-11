cffb27bc3d8142b26a10bd8d1a0ebba09a740b2e
Marcelo Sousa <marcelo.sousa@lbca.com.br>
2019-06-11 18:38:55 -0300

    Usando metodos #6

--- script.js
+++ script.js
@@ -1,42 +1,19 @@
 //Instanciando objeto do vue
-/*let placar = new Vue({
-	el: '#placar', //Defenindo id do elemento	
+let app = new Vue({
+	el: '#app', //Defenindo id do elemento	
 	data:{ 
-		n: 0
+		nome:'Marcelo',
+		idade:21
+	},
+	methods:{
+		mostrar : (nome,idade)=>{
+			let txt = `Olá, ${nome}  - ${idade} anos`;
+			return txt;
+		},
+		testar : ()=>{
+			return 'testando 1,2,3..';
+		}
 	}
 })
 
-
-let lista = new Vue({
-	el: '#lista', //Defenindo id do elemento	
-	data:{ 
-		nomes: [
-			{nome:'Marcelo',sobrenome: 'Sousa', idade: '21'},
-			{nome:'Antonio',sobrenome: 'da Silva', idade: '18'},
-			{nome:'Paulo',sobrenome: 'de Tasso', idade: '25'},
-			{nome:'Maria',sobrenome: 'de Sousa', idade: '23'},
-				
-		]
-	}
-})
-
-
-let app = new Vue ({
-	el: '#app',
-	data:{
-		texto: 'Texto Qualquer',
-		n1: 10,
-		n2: 15,
-		n3: 0
-	}
-})*/
-
-let soma = new Vue({
-	el: '#soma',
-	data:{
-		n1: Math.floor(Math.random()*30),
-		n2: Math.floor(Math.random()*30),
-		resultado: 0
-	}
-})
 
