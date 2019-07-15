77b2e0b90a5de47821eada913385387e8a204167
Marcelo Sousa <marcelo.sousa@lbca.com.br>
2019-07-15 19:22:50 -0300

    Renderização condicional #24

--- script.js
+++ script.js
@@ -57,19 +57,7 @@ let app = new Vue({
 let app = new Vue ({
 	el: '#app',
 	data:{
-		aviso:'Alguma mensagem de aviso',
-		style:{
-			avisoBase:{
-				border:'1px solid #000',
-				fontSize: '20px',
-				padding: '10px'
-			},
-			avisoError:{
-				backgroundColor:'#FF0000',
-				color: '#FFF',
-				fontSize: '30px'
-			}
-		}
+		ganhou:true
 	}
 	
 });
