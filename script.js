d360e0409dbe966a97e61fcf215d9e11199805ca
Marcelo Sousa <marcelo.sousa@lbca.com.br>
2019-07-30 18:14:11 -0300

    Componente com ação simples

--- script.js
+++ script.js
@@ -63,6 +63,20 @@ Vue.component('menu-superior',{
 })
 
 
+Vue.component('contador',{
+	data:function(){
+		return {
+			c:0
+		};
+	},
+	methods:{
+		aumentar:function(){
+			this.c++;
+		}
+	},
+	template: '<div><span>{{c}}x</span> <button v-on:click="aumentar">Aumentar </button> </div>'
+})
+
 let app = new Vue ({
 	el: '#app',
 	data:{
